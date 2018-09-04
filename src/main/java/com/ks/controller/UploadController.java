5b785b368fc418ccd419c5fa29d257be4eb0f2a9
jxzhang <a559927z@163.com>
2018-09-04 18:52:48 +0800

    [+]upload

--- src/main/java/com/ks/controller/UploadController.java
+++ src/main/java/com/ks/controller/UploadController.java
@@ -1,18 +1,24 @@
 package com.ks.controller;
 
-import com.ks.dto.BaseDto;
+import com.ks.utils.StringUtil;
+import groovy.util.logging.Slf4j;
+import net.chinahrd.utils.ExcelUtil;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
-import org.apache.commons.io.IOUtils;
 
 /**
  * Title: ${type_name} <br/>
@@ -24,8 +30,9 @@ import org.apache.commons.io.IOUtils;
  * @Verdion 1.0 版本
  * ${tags}
  */
-@RequestMapping("/admin/upload")
+@Slf4j
 @Controller
+@RequestMapping("/admin/upload")
 public class UploadController extends BaseController {
 
     /**
@@ -49,22 +56,63 @@ public class UploadController extends BaseController {
      * @return
      */
     @ResponseBody
-    @RequestMapping(value = "/parseXls2Dto", method = {RequestMethod.GET, RequestMethod.POST})
-    public String parseXls2Dto(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            @SuppressWarnings("unchecked")
-            List<String> lines = IOUtils.readLines(request.getInputStream());
-            StringBuilder sb = new StringBuilder();
-            for (String str : lines) {
-                sb.append(str);
+    @RequestMapping(value = "parseXls2Dto", method = {RequestMethod.GET, RequestMethod.POST})
+    public String parseXls2Dto(MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws IOException, InvalidFormatException {
+        // jar包的路径
+        String filePath = "";
+        if (!file.isEmpty()) {
+            File temp = new File("");
+            filePath = temp.getAbsolutePath() + "\\" + file.getOriginalFilename();
+            BufferedOutputStream out;
+            try {
+                out = new BufferedOutputStream(new FileOutputStream(new File(filePath)));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+                System.out.println("上传成功");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("上传失败");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                System.out.println("上传失败");
             }
-//            BaseDto dto = JSON.parse(sb.toString(), BaseDto.class);
-//            System.out.println(com.alibaba.fastjson.JSON.toJSONString(dto));
-            return "成功";
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return "失败";
+
+        Workbook sheets = WorkbookFactory.create(file.getInputStream());
+        Sheet sheet = sheets.getSheetAt(0);
+        for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            Cell cell = row.getCell(0);
+            System.out.println(cell);
+
+            String stringCellValue = cell.getStringCellValue();
+            if (StringUtil.containsAny(stringCellValue, "☐")) {
+                System.out.println("答案=====>" + stringCellValue);
+                if (StringUtil.containsAny(stringCellValue, "☑")) {
+                    String ss = stringCellValue
+                            .replaceAll(" ", "")
+                            .replaceAll("   ", "");
+
+                    ss = StringUtil.substringAfter(ss, "☑");
+                    ss = StringUtil.substringBefore(ss, "☐");
+                    ss = StringUtil.deleteWhitespace(ss).trim();
+                    System.out.println("正确答案=====>" + ss);
+                }
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String ss = "☐I、II   ☑I、II、III   ☐II、III   ☐I、III "
+                .replaceAll(" ", "")
+                .replaceAll("   ", "");
+
+        ss = StringUtil.substringAfter(ss, "☑");
+        ss = StringUtil.substringBefore(ss, "☐");
+        ss = StringUtil.deleteWhitespace(ss).trim();
+
+        System.out.println(ss);
     }
 }
-
---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,8 @@
+    //    js通过model取值
+    var currentTime = [[${currentTime}]];
+    console.log(currentTime);
+
+    $(function () {
+        var text = $("#jqueryTest").text('abc');
+        console.log(text);
+    });
