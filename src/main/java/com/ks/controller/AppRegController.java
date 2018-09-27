1ec16bb0d1eb0f85911380bae254f7c6036a6581
ASUS\Administrator <a559927z@163.com>
2018-09-28 00:37:49 +0800

    [+]绑定用户入库

--- src/main/java/com/ks/controller/AppRegController.java
+++ src/main/java/com/ks/controller/AppRegController.java
@@ -2,9 +2,13 @@ package com.ks.controller;
 
 import com.ks.dao.ExamQuestionBankYaMapper;
 import com.ks.dao.ExamUserAnswerYaMapper;
-import com.ks.dto.KVItemDto;
-import com.ks.dto.RegUserDto;
+import com.ks.dao.PublicUserInfoMapper;
+import com.ks.dto.*;
+import com.ks.util.ShiroUtils;
+import lombok.extern.slf4j.Slf4j;
+import net.chinahrd.utils.Identities;
 import net.chinahrd.utils.verifyCode.VerifyCodeUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -21,6 +25,7 @@ import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Title: ${type_name} <br/>
@@ -32,16 +37,14 @@ import java.io.IOException;
  * @Verdion 1.0 版本
  * ${tags}
  */
+@Slf4j
 @RequestMapping("/app/reg")
 @Controller
 public class AppRegController {
 
 
-    @Value("${ctx}")
-    private String ctx;
-
     @Autowired
-    private ExamQuestionBankYaMapper examQuestionBankYaMapper;
+    private PublicUserInfoMapper publicUserInfoMapper;
     @Autowired
     private ExamUserAnswerYaMapper examUserAnswerYaMapper;
 
@@ -70,12 +73,12 @@ public class AppRegController {
         response.setContentType("image/jpeg");
 
         // 生成随机字串
-        String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+        String verifyCode = VerifyCodeUtils.generateVerifyCode(4).toLowerCase();
         // 存入会话session
         HttpSession session = request.getSession(true);
         // 删除以前的
-        session.removeAttribute("verCode");
-        session.setAttribute("verCode", verifyCode.toLowerCase());
+        session.removeAttribute("verCode_" + verifyCode);
+        session.setAttribute("verCode_" + verifyCode, verifyCode);
 
         // 生成图片
         int w = 162, h = 45;
@@ -86,17 +89,47 @@ public class AppRegController {
     @PostMapping
     @ResponseBody
     @RequestMapping("/regUser")
-    public KVItemDto<Boolean, Object> regUser(RegUserDto regUserDto) {
+    public KVItemDto<Boolean, Object> regUser(HttpServletRequest request, RegUserDto regUserDto) {
         KVItemDto<Boolean, Object> rs = new KVItemDto<>();
         // check verificationCode
-
+        String verificationCode = regUserDto.getVerificationCode().toLowerCase();
+        HttpSession session = request.getSession(true);
+        Object sessionVerCode = session.getAttribute("verCode_" + verificationCode);
+        if (null == sessionVerCode) {
+            log.error("验证码不正确");
+            return null;
+        }
         // check exists user
+        String phone = regUserDto.getPhone();
+        PublicUserInfoExample example = new PublicUserInfoExample();
+        example.createCriteria().andEnNameEqualTo(phone);
+        List<PublicUserInfo> exists = publicUserInfoMapper.selectByExample(example);
+        if (CollectionUtils.isNotEmpty(exists)) {
+            log.error("账号已经存在");
+            return null;
+        }
+
+        String pwd = regUserDto.getPwd();
+        String salt = Identities.uuid3();
+        PublicUserInfo dto = new PublicUserInfo();
+        dto.setUid(Identities.uuid2());
+        dto.setEnName(phone);
+        dto.setPassword(ShiroUtils.shiroMd5Hash(pwd, salt, 2));
+        dto.setSalt(salt);
+        dto.setState(1);
+        try {
+            // reg success
+            publicUserInfoMapper.insertSelective(dto);
+            rs.setK(true);
+            rs.setV("绑定成功");
+            return rs;
+        } catch (Exception e) {
+            rs.setK(false);
+            rs.setV("绑定失败");
+            return rs;
+        }
 
 
-        // reg success
-        System.out.println(regUserDto);
-
-        return rs;
     }
 
 }
