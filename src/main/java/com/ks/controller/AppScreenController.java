61ba20e7110bd24638b0377d223911e60354ad43
jxzhang <a559927z@163.com>
2018-10-08 18:37:28 +0800

    [*]body style 调用公共 [+]screenTiApp.html

---
+++ src/main/java/com/ks/controller/AppScreenController.java
@@ -0,0 +1,31 @@
+package com.ks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Title: ${type_name} <br/>
+ * <p>
+ * Description: <br/>
+ *
+ * @author jxzhang
+ * @DATE 2018年09月17日 22:53
+ * @Verdion 1.0 版本
+ * ${tags}
+ */
+@RequestMapping("/app/screen")
+@Controller
+public class AppScreenController {
+
+
+    /**
+     * http://localhost:8080/exam/app/screen/toIndex
+     *
+     * @return
+     */
+    @RequestMapping("/toIndex")
+    public String toScreenTi() {
+        return "app/screenTiApp";
+    }
+
+}
